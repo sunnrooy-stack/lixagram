8a2c2b57f93805b12696189853c7231f38f500b1
sunnrooy-stack <sunnrooy@gmail.com>
2026-05-26 09:20:16 -0700

    Update socket.js

--- backend/socket.js
+++ backend/socket.js
@@ -6,7 +6,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin:"https://lixagram.onrender.com",
+        origin:/https://lixagram.onrender.com/,
         methods: ["GET", "POST"]
     }
 })
