1a383f76dbdac0cf1e162610f1dd36982293c454
sunnrooy-stack <sunnrooy@gmail.com>
2026-05-27 05:49:56 -0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ dotenv.config()
 
 const port=process.env.PORT || 5000
 app.use(cors({
-    origin:"https://lixagram.onrender.com"],
+    origin:"https://lixagram.onrender.com",
     credentials:true
 }))
 app.use((req, res, next) => {
